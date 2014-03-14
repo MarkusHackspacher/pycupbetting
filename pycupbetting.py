a000030d53aefdf707ac22cffe91dab5eda3c036
MarkusHackspacher <hackspacher@gmx.de>
2014-03-15 00:26:24 +0100

    new file for menu

--- pycupbetting.py
+++ pycupbetting.py
@@ -1,53 +1,33 @@
-#!/usr/bin/env python
-
-#
-#  Copyright (C) 2012  Christian Hausknecht
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
-    ~~~~~~~~~~~~~
-    classymenu.py
-    ~~~~~~~~~~~~~
+pycupbetting, load module lotto
 
-    The most advanced module to create and handle menus with basic Python
-    kwnoledges.
+Copyright (C) <2014> Markus Hackspacher
 
-    This time we use a class to represent and handle our menu tasks.
+This file is part of pycupbetting.
 
-    It might be understandable even for newbies, but you should have been
-    familiar to classes in Python.
+pyLottoverwaltung is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    Of course the core ideas do not differ from the other, structure and
-    function based approaches within this folder. As you can see a class is
-    often the more comfortable way to handle deeply nested data structures.
+pycupbetting is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-    Our latest approach in `metamenu.py` handles tuples in a dictionary
-    in a dictionary... that ist not very comfortable to deal with. You can
-    get confused by indexes and perhaps keys. It feels less clumsy to deal
-    with attributes of a class.
-
-    .. moduleauthor:: Christian Hausknecht <christian.hausknecht@gmx.de>
+You should have received a copy of the GNU General Public License
+along with pycupbetting.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-import sys
-from itertools import chain
 
 from modules import model
 from sqlalchemy import orm
 from sqlalchemy import create_engine
+import functools
+
+from classymenu import Menu
 
 # Create an engine and create all the tables we need
 engine = create_engine('sqlite:///dbcupbetting.sqlite', echo=False)
@@ -58,6 +38,23 @@ model.Base.metadata.create_all(engine)
 sm = orm.sessionmaker(bind=engine, autoflush=True, autocommit=True,
     expire_on_commit=True)
 session = orm.scoped_session(sm)
+
+
+class selection_menu():
+    def __init__(self, datatable):
+        self.selection_id = 0
+        select = Menu("Auswahlmenü")
+        for entry in datatable:
+            get_id = functools.partial(self.get_id, entry.id)
+            select.append(entry.name, get_id)
+        select.finish(text="Zurück")
+        select.run(once=True)
+
+    def __int__(self):
+        return self.selection_id
+
+    def get_id(self, number):
+        self.selection_id = number
 
 
 def info():
@@ -85,16 +82,12 @@ def edit_user(user):
 def new_user():
     session.add(edit_user(model.User()))
 
+
 def select_user():
-    userid = []
-    for all_user in session.query(model.User).all():
-        print ("User Nummer: {} Name: {}, Vollername: {}, ".
-            format(all_user.id, all_user.name, all_user.fullname))
-        userid.append(all_user.id)
-    select_id = input('User Nummer:')
-    if not (int(select_id) in userid):
+    userid = int(selection_menu(session.query(model.User).all()))
+    if userid == 0:
         return
-    user = session.query(model.User).filter_by(id=select_id).first()
+    user = session.query(model.User).filter_by(id=userid).first()
 
     def editor_user():
         edit_user(user)
@@ -109,6 +102,7 @@ def select_user():
     userselect.finish()
     userselect.run()
 
+
 def edit_team(team):
     default = team.name
     team.name = input("Team Name [{}]:".format(default))
@@ -120,16 +114,12 @@ def edit_team(team):
 def new_team():
     session.add(edit_team(model.Team()))
 
+
 def select_team():
-    teamid = []
-    for all_teams in session.query(model.Team).all():
-        print ("Team Nummer: {} Name: {}".
-            format(all_teams.id, all_teams.name))
-        teamid.append(all_teams.id)
-    select_id = input('Team Nummer:')
-    if not (int(select_id) in teamid):
+    teamid = int(selection_menu(session.query(model.Team).all()))
+    if teamid == 0:
         return
-    team = session.query(model.Team).filter_by(id=select_teamid).first()
+    team = session.query(model.Team).filter_by(id=teamid).first()
 
     def editor_team():
         edit_team(team)
@@ -137,134 +127,14 @@ def select_team():
     def info_team():
         print ("Teamname: {}".format(team.name))
 
-    teamselect = Menu("Teameditormenü")
+    teamselect = Menu("Teameditormenü {}".format(team.name))
     teamselect.append("Teamnamen ändern", editor_team)
     teamselect.append("Teaminfo", info_team)
     teamselect.finish()
     teamselect.run()
 
 
-class Menu:
-    """
-    Class that represents and handles a complete menu system all in once.
-
-    Each menu class can...
-
-        ... print out the (current) menu
-        ... handle the user input
-        ... run in an event-loop, that handles the complete menu based
-            workflow.
-
-    It provides the `finish`-method, which adds automatically all needed
-    'Exit'-entries to each (sub)menu, so building up a menu is not so much
-    typing.
-
-    To avoid recursion, we use the `context`-attribute of the Menu-class. The
-    current menu-object is always bound to this attribute. As we can combine
-    arbitrary menu objects (via the `append_submenu`-method) we must keep
-    the current working menu accessible to operate only in one object, the
-    'root'-object.
-    """
-
-    def __init__(self, title):
-        self.title = title
-        self.items = []
-        self.context = self
-
-    def __repr__(self):
-        return "Menu({})".format(self.title)
-
-    def __str__(self):
-        head = ("", "-" * len(self.title), "{}".format(self.title),
-                "-" * len(self.title))
-        entries = ("{} {} {}".format(
-                        index, "+" if isinstance(entry[1], Menu) else " ",
-                        entry[0]
-                    )
-                    for index, entry in enumerate(self, 1)
-        )
-        return "\n".join(chain(head, entries))
-
-    def __getitem__(self, key):
-        """
-        Nice to have method for providing the iterable "interface". So you can
-        access any menu item via an index or loop over all entries using
-        `for` :-)
-        """
-        return self.items[key]
-
-    def append(self, text, func):
-        """
-        Appends a menu entry to `self.items`.
-
-        :param text: string with entry description
-        :param func: callable that will be called if chosen
-        """
-        self.items.append((text, func))
-
-    def append_submenu(self, other):
-        """
-        Appends a submenu entry to the menu. That can be a complex branch
-        of menu-objects.
-
-        :param other: Menu object, that represents a complete submenu-branch.
-        """
-        self.items.append((other.title, other))
-
-    def finish(self, text="Exit"):
-        """
-        Nice helper method that computes all needed 'Exit'-items for each
-        submenu and the 'final' 'Exit'-item at the main-menu, which is simply
-        any string.
-
-        All other exit-items are menu objects of the corresponding parent-menu.
-
-        The algorithm implements a breadth-first search for handling this task.
-
-        :param text: string with the text of the 'Exit'-item.
-        """
-        self.items.append((text, "#exit"))
-        stack = [(self, None)]
-        while stack:
-            menu, parent = stack.pop()
-            for _, command in menu:
-                if isinstance(command, Menu):
-                    stack.append((command, menu))
-            if menu is not self:
-                menu.append("{} -> {}".format(text, parent.title), parent)
-
-    def get_user_input(self):
-        while True:
-            try:
-                choice = int(input("Ihre Wahl?: ")) - 1
-                if 0 <= choice < len(self.context.items):
-                    return choice
-                else:
-                    raise IndexError
-            except (ValueError, IndexError):
-                print("Bitte nur Zahlen aus dem Bereich 1..{} eingeben".format(
-                                                    len(self.context.items)))
-
-    def run(self):
-        """
-        Core method to handle a menu. This invokes a loop that handles all
-        menu tasks as long as the main menu is exited by the user.
-        """
-        while True:
-            print(self.context)
-            choice = self.get_user_input()
-            _, command = self.context[choice]
-            if isinstance(command, Menu):
-                self.context = command
-            elif isinstance(command, str):
-                return
-            else:
-                command()
-
-
 def main():
-    # We build up some demonstration menu as in the other menu systems.
-    # Now we can use classes to create menu objects...
     menu = Menu("Hauptmenü")
     menu.append("Info", info)
 
