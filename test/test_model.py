becb876731008f232d6cf7ef0e3422f5ec861e94
Markus Hackspacher <hackspacher@gmx.de>
2022-11-17 16:00:51 +0100

    change from .modules import model

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,7 @@
 
 # pycupbetting
 
-# Copyright (C) <2015> Markus Hackspacher
+# Copyright (C) <2015,2022> Markus Hackspacher
 
 # This file is part of pycupbetting.
 
@@ -22,7 +22,7 @@
 import unittest
 from sqlalchemy import orm
 from sqlalchemy import create_engine
-from modules import model
+from .modules import model
 
 
 class TestCodeFormat(unittest.TestCase):
